d1beaf16247e5665a2c94d517a0c581cc2cc609b
Tejash-Katuwal <tejashkatuwal99@gmail.com>
2025-05-01 23:36:24 +0545

    Organization Overview-3

--- service/views.py
+++ service/views.py
@@ -167,7 +167,7 @@ class AdminOrganizationOverviewView(APIView):
     def get(self, request):
         try:
             print(request.user)
-            if not request.user.is_superuser:
+            if not (request.user.is_superuser or request.user.is_staff):
                 return Response({
                     "error": "Only superadmins and staffs can access this data."
                 }, status=status.HTTP_403_FORBIDDEN)
