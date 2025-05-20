32712fc49c96bc9202bde32e6f885e4c4b52b62d
Tejash-Katuwal <tejashkatuwal99@gmail.com>
2025-05-20 16:32:19 +0545

    quota-1

--- payment/urls.py
+++ payment/urls.py
@@ -4,5 +4,5 @@ from .views import PaymentSuccessView, ChatbotPaymentListView, ChatbotQuotaView
 urlpatterns = [
     path('payment-success/', PaymentSuccessView.as_view(), name='payment-success'),
     path('chatbot-payments/', ChatbotPaymentListView.as_view(), name='chatbot-payments'),
-    path('chatbot-quota/', ChatbotQuotaView.as_view(), name='Chatbot Quota'),
+    path('/<int:chatbot_id>/chatbot-quota/', ChatbotQuotaView.as_view(), name='Chatbot Quota'),
 ]
